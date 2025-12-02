b951b2447729883797cf85112668b03d9ef0f708
vetrivelan01 <162667377+vetrivelan01@users.noreply.github.com>
2025-12-02 12:39:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string day = "Wednesday";
+
+        switch (day)
+        {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                Console.WriteLine("Weekdays");
+                break;
+            case "Saturday":
+            case "Sunday":
+                Console.WriteLine("Weekend");
+                break;
+
+            default:
+                Console.WriteLine("Invalid day");
+                break;
+        }
+    }
+}
+
