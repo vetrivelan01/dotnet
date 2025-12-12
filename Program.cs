93652f9c598de9fd5624fe972dc45fbdefafda89
vetrivelan01 <162667377+vetrivelan01@users.noreply.github.com>
2025-12-12 10:24:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+class Program
+{
+  static void Main()
+    {
+        int[,] mat =
+        {
+            {10,20,30,40 },
+            {15,25,35,45 },
+            {27,29,37,48},
+            {32,33,39,50 }
+
+        };
+        int n = mat.GetLength(0);
+        for(int i=0; i<n;i++)
+            if(i%2==0)
+
+        {
+                for (int j = 0; j < n; j++)
+
+                    Console.WriteLine(mat[i, j]+ " ");
+                }
+                else{
+                    for (int j = n - 1; j <= 0; j--)
+                    
+                        Console.WriteLine(mat[i, j] + " ");
+                    
+        }
+       
+
+
+        }
+        
+    }
